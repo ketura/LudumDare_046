4fdcb48fdc1dc7c9b053669c377c7e97a44f0500
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2020-04-18 23:26:49 -0500

    Created art for ants.  Created scene hierarchy.

---
+++ src/LudumDare_046/Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+  public string Name;
+  public string Tooltip;
+
+  public Color Color;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
+}
---
+++ src/LudumDare_046/Assets/Scripts/Cards/GeneCard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GeneTier
+{
+	One,
+	Two,
+	Alpha,
+	Beta,
+	Gamma,
+	Ultimate
+}
+
+public class GeneCard : Card
+{
+	public GeneTier Tier;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
